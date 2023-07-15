65021a636c5721025578ef05b56635ebf27b305d
Ursx2204 <127949449+Yurii2276@users.noreply.github.com>
2023-07-15 17:10:58 +0300

    task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,29 @@
+
+const btnChangeColor = document.querySelector('.change-color');
+const valueColor = document.querySelector('.color');
+
+btnChangeColor.addEventListener(`click`, changeBodyColor); 
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+ 
+function changeBodyColor() {
+  const randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomColor;
+  valueColor.textContent = randomColor;
+
+}
+
+
+
+
+
+
+// Напиши скрипт, який змінює кольори фону
+// елемента < body > через інлайн - стиль 
+// по кліку на button.change - color і 
+// виводить значення кольору 
+// в span.color.
